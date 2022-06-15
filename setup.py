e57f1f57d68c775c2740ee61d3b1b56f0f54aa49
chuksoo <mexx4me@yahoo.com>
2022-06-14 23:17:50 -0800

    Initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This portfolio contains my ongoing and completed machine learning and artificial intelligence projects. The repo also shows my expertise in ML/AI, tool stack, technical skills, and tools used for the projects.',
+    author='Chukwuemeka Okoli',
+    license='MIT',
+)
